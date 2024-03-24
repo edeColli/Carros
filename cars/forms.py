12f08ec74d1bd5719c576e49481be8c49552597c
Ednesio <ednesio.colli@gmail.com>
2024-03-24 20:17:07 -0300

    Aprendendo Signals

--- cars/forms.py
+++ cars/forms.py
@@ -6,14 +6,14 @@ class CarModelForm(forms.ModelForm):
         model = Car
         fields = '__all__'
 
-    def clean_value(self):
-        value = self.cleaned_data.get('value')
+    # def clean_value(self):
+    #     value = self.cleaned_data.get('value')
 
-        if value < 10000:
-            self.add_error('value', 'Valor mínimo do carro deve ser superior a R$ 10.000,00!')
+    #     if value < 10000:
+    #         self.add_error('value', 'Valor mínimo do carro deve ser superior a R$ 10.000,00!')
 
-    def clean_factory_year(self):
-        factory_year = self.cleaned_data.get('factory_year')
+    # def clean_factory_year(self):
+    #     factory_year = self.cleaned_data.get('factory_year')
 
-        if factory_year < 1975:
-            self.add_error('factory_year', 'Não é permitido carros inferiores a 1975!')
+    #     if factory_year < 1975:
+    #         self.add_error('factory_year', 'Não é permitido carros inferiores a 1975!')
