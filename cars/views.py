a2b9ffd769135fa824d37ddb482d4507ac3df754
Ednesio <ednesio.colli@gmail.com>
2024-03-24 17:53:41 -0300

    Adicionando camada de autenticação

--- cars/views.py
+++ cars/views.py
@@ -1,10 +1,9 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class CarsListView(ListView):
@@ -18,18 +17,19 @@ class CarsListView(ListView):
         if search:
             cars = cars.filter(model__icontains=search)
         return cars
+class CarDetailView(DetailView):
+    model = Car    
+    template_name = 'car_detail.html'
 
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class NewCarCreateView(CreateView):
     model = Car
     form_class = CarModelForm
     template_name = 'new_car.html'
     success_url = '/cars/'
 
-class CarDetailView(DetailView):
-    model = Car    
-    template_name = 'car_detail.html'
 
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
@@ -38,7 +38,7 @@ class CarUpdateView(UpdateView):
     def get_success_url(self):
         return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
 
-
+@method_decorator(login_required(login_url='login'), name='dispatch')
 class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
